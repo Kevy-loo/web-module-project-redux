0e4efed3fabeaf3cf84ebbc74eace240b5ea9014
kevin-liu <liu.kevin1997@gmail.com>
2022-01-19 13:58:24 -0500

    finished up the last touches of the mvp

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
+import favoritesReducer from './favoritesReducer';
 
 import movieReducer from './movieReducer';
+const rootReducer = combineReducers({
+    movieReducer,
+    favoritesReducer
+})
+export default rootReducer;
 
-export default movieReducer;
